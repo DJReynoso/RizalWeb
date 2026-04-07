933e785fa7351dee4d24a39527f3159dfa4c4afc
Daniel Jess Reynoso <danieljess.reynoso@gmail.com>
2026-04-07 21:43:49 +0800

    Added fx and transitions.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,86 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import NavigationBar from "./components/NavigationBar/NavigationBar";
 import Home from "./components/Home/Home";
 import Life from "./components/Life/Life";
 import Works from "./components/Works/Works";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
+import { motion, AnimatePresence } from "framer-motion";
+import smokeVideo from "./assets/smokeTransitionAnimation.mp4";
+
+const pageVariants = {
+  initial: {
+    opacity: 0,
+    scale: 0.8,
+    filter: "blur(10px)",
+  },
+  enter: {
+    opacity: 1,
+    scale: 1,
+    filter: "blur(0px)",
+    transition: {
+      duration: 0.8,
+      ease: "easeOut",
+    },
+  },
+  exit: {
+    opacity: 0,
+    scale: 1.2,
+    filter: "blur(20px)",
+    transition: {
+      duration: 0.8,
+      ease: "easeIn",
+    },
+  },
+};
 
 function App() {
+  const location = useLocation();
+  const [showSmoke, setShowSmoke] = useState(false);
+  const videoRef = React.useRef(null);
+
+  const handlePageChange = () => {
+    setShowSmoke(true);
+    if (videoRef.current) {
+      videoRef.current.currentTime = 0;
+      videoRef.current.play();
+    }
+  };
+
+  const handleVideoEnd = () => {
+    setShowSmoke(false);
+  };
+
   return (
     <>
       <div className="navigation-bar-container">
-        <NavigationBar />
+        <NavigationBar onNavigate={handlePageChange} />
       </div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/life" element={<Life />} />
-        <Route path="/works" element={<Works />} />
-      </Routes>
+      {showSmoke && (
+        <video
+          ref={videoRef}
+          src={smokeVideo}
+          className="smoke-video"
+          muted
+          autoPlay
+          onEnded={handleVideoEnd}
+        />
+      )}
+      <AnimatePresence mode="wait">
+        <motion.div
+          key={location.pathname}
+          variants={pageVariants}
+          initial="initial"
+          animate="enter"
+          exit="exit"
+        >
+          <Routes location={location}>
+            <Route path="/" element={<Home />} />
+            <Route path="/life" element={<Life />} />
+            <Route path="/works" element={<Works />} />
+          </Routes>
+        </motion.div>
+      </AnimatePresence>
     </>
   );
 }
--- client/src/components/NavigationBar/NavigationBar.jsx
+++ client/src/components/NavigationBar/NavigationBar.jsx
@@ -1,8 +1,17 @@
 import "./NavigationBar.css";
 import { NavLink } from "react-router-dom";
 import { Navbar, Container, Nav } from "react-bootstrap";
+import gunshotSound from "../../assets/gunshot.mp3";
 
-function NavigationBar() {
+function NavigationBar({ onNavigate }) {
+  const playGunshotSound = () => {
+    const audio = new Audio(gunshotSound);
+    audio.play();
+    if (onNavigate) {
+      onNavigate();
+    }
+  };
+
   return (
     <>
       <Navbar bg="black" data-bs-theme="dark" expand="lg" sticky="top">
@@ -16,19 +25,35 @@ function NavigationBar() {
               fontWeight: "bold",
               letterSpacing: "0.1em",
             }}
+            onClick={playGunshotSound}
           >
             RIZAL
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link as={NavLink} to="/" className="fs-6 ms-3">
+              <Nav.Link
+                as={NavLink}
+                to="/"
+                className="fs-6 ms-3"
+                onClick={playGunshotSound}
+              >
                 Home
               </Nav.Link>
-              <Nav.Link as={NavLink} to="/life" className="fs-6 ms-3">
+              <Nav.Link
+                as={NavLink}
+                to="/life"
+                className="fs-6 ms-3"
+                onClick={playGunshotSound}
+              >
                 Life
               </Nav.Link>
-              <Nav.Link as={NavLink} to="/works" className="fs-6 ms-3">
+              <Nav.Link
+                as={NavLink}
+                to="/works"
+                className="fs-6 ms-3"
+                onClick={playGunshotSound}
+              >
                 Works
               </Nav.Link>
             </Nav>
